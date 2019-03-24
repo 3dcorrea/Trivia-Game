926c1cb3c12adeec0c97a310777f187caa2b8b0e
Dan Correa <dancorrea3@gmail.com>
2019-03-23 20:54:03 -0400

    added additional questions

--- asset_1/javascript/app.js
+++ asset_1/javascript/app.js
@@ -106,46 +106,45 @@ var trivia = {
 }
 
 // array of objects with the questions, possible answers, and the correct answer
-
-
 var questionBank = [{
     question: "Who is the strongest?",
-    answers: {
-      a: "Cosmic Armor Superman with a Super Mario \"star\" powerup",
-      b: "Shaggy at 1% power after a rough night of drinking",
-      c: "Thanos with Infinity Gauntlet and a \"Reverse\" Uno card",
-      d: "Mastered Ultra Instinct Gogeta with Rinnegan and a 1st Edition Charizard"
-    },
-    correctAnswer: "b"
+    answers: ["Cosmic Armor Superman with a Super Mario \"star\" powerup", "Shaggy at 1% power after a rough night of drinking", "Thanos with Infinity Gauntlet and a \"Reverse\" Uno card", "Mastered Ultra Instinct Gogeta with Rinnegan and a 1st Edition Charizard"],
+    correct: "Shaggy at 1% power after a rough night of drinking"
   },
+
   {
     question: "What is the greatest website ever created?",
-    answers: {
-      a: "Google.com",
-      b: "W3schools.com",
-      c: "Placecage.com",
-      d: "getbootstrap.com"
-    },
-    correctAnswer: "c"
+    answers: ["Google.com", "W3schools.com", "Placecage.com", "getbootstrap.com"],
+    correct: "Placecage.com"
+
   },
   {
     question: "Where has Waldo REALLY been all these years?",
-    answers: {
-      a: "At the last Roy Rogers in existence",
-      b: "In rehab",
-      c: "At Starbucks condescendingly sipping an $18 fair trade pour-over, continuously pushing to master out of spite",
-      d: "Looking for Carmen Sandiego"
-    },
-    correctAnswer: "d"
+    answers: ["At the last Roy Rogers in existence", "In rehab for \"substance abuse\"", "At Starbucks condescendingly sipping an $18 fair trade pour-over, continuously pushing to master out of spite", "Looking for Carmen Sandiego"],
+    correct: "Looking for Carmen Sandiego"
+
   },
   {
     question: "In 1978, Superman teamed up with what well-known persona to defeat an alien invasion?",
-    answers: {
-      a: "Charles Manson",
-      b: "A resurrected Benito Mussolini",
-      c: "Muhammad Ali",
-      d: "Ted Bundy"
-    },
-    correctAnswer: "c"
+    answers: ["Charles Manson", "A resurrected Benito Mussolini", "Muhammad Ali", "Ted Bundy"],
+    correct: "Muhammad Ali"
+
+  },
+  {
+    question: "Rounded to the nearest ten thousand square miles, what is the area of Idaho?",
+    answers: ["82,751 square miles", "20 square miles", "520,000 square miles", "9001 square miles"],
+    correct: "82,751 square miles"
+
+  },
+  {
+    question: "In China, animals are forbidden to use human language. This belief led to the ban of a book of which famous writer, in the country?",
+    answers: ["E.L. James,\"50 Shades of Grey\"", "Miguel de Cervantes, \"Don Quixote\"", "Lewis Carroll, \"Alice's Adventures in Wonderland\"", "Vātsyāyana, \"Kama Sutra\""],
+    correct: "Lewis Carroll, \"Alice's Adventures in Wonderland\""
+  },
+  {
+    question: "Why does my wife do that weird thing with her cheek?",
+    answers: ["Don't know, don't care", "She's having a stroke", "She's severely dehydrated", "I understand that this is a freebee question, so I'll pick this one"],
+    correct: "I understand that this is a freebee question, so I'll pick this one"
   }
-];
+
+]
